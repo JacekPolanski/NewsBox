0cc473c522d1d4b3c4edc8168de564f9b21031ff
Jacek Polański <kontakt@jpolanski.pl>
2016-04-03 15:58:23 +0200

    Entity - tabs change to spaces

--- src/AppBundle/Entity/News.php
+++ src/AppBundle/Entity/News.php
@@ -7,96 +7,96 @@ use JMS\Serializer\Annotation\Inline;
 
 class News
 {
-	/**
-	 * @var int
-	 *
-	 * @Type("integer")
-	 */
-	private $id;
+    /**
+     * @var int
+     *
+     * @Type("integer")
+     */
+    private $id;
 
-	/**
-	 * @var string
-	 *
-	 * @Type("string")
-	 */
-	private $title;
+    /**
+     * @var string
+     *
+     * @Type("string")
+     */
+    private $title;
 
-	/**
-	 * @var \DateTime
-	 *
-	 * @Type("DateTime<'Y-m-d H:i'>")
-	 */
-	private $time;
+    /**
+     * @var \DateTime
+     *
+     * @Type("DateTime<'Y-m-d H:i'>")
+     */
+    private $time;
 
-	/**
-	 * @var string
-	 *
-	 * @Type("string")
-	 */
-	private $content;
+    /**
+     * @var string
+     *
+     * @Type("string")
+     */
+    private $content;
 
-	/**
-	 * @return string
-	 */
-	public function getTitle()
-	{
-		return $this->title;
-	}
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	/**
-	 * @param string $title
-	 */
-	public function setTitle($title)
-	{
-		$this->title = $title;
-	}
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
 
-	/**
-	 * @return \DateTime
-	 */
-	public function getTime()
-	{
-		return $this->time;
-	}
+    /**
+     * @return \DateTime
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
 
-	/**
-	 * @param \DateTime $time
-	 */
-	public function setTime($time)
-	{
-		$this->time = $time;
-	}
+    /**
+     * @param \DateTime $time
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getContent()
-	{
-		return $this->content;
-	}
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
 
-	/**
-	 * @param string $content
-	 */
-	public function setContent($content)
-	{
-		$this->content = $content;
-	}
+    /**
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @param int $id
-	 */
-	public function setId($id)
-	{
-		$this->id = $id;
-	}
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
 }
